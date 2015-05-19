4fb3b819d8de88d281ded594bef66d4ae88641e4
Brian Harbron <harbron@gmail.com>
2015-05-19 19:47:30 +0000

    Added start of main() method

--- snippets.py
+++ snippets.py
@@ -29,4 +29,12 @@ def remove(name):
     """
     logging.error("FIXME: Unimplemented - remove({!r})".format(name))
     return name
-    
+  
+def main():
+    """Main function"""
+    logging.info("Constructing parser")
+    parser = argparse.ArgumentParser(description="Store and retrieve snippets of text")
+    arguments = parser.parse_args(sys.argv[1:])
+
+if __name__ == "__main__":
+    main()
